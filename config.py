40216cfc84f99817198c8ae387f38caa9682638a
MissMusicBot <khatriuzef197@gmail.com>
2025-06-25 12:25:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 class Config:
     API_ID = "20136640"
     API_HASH = "32fa482ffd34ceefec26b7ccd73c4fbb"
-    #TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
+    #TOKEN = "7591932559:AAFh4dga16Nw-VSE7MbeysMiCrC7tSsD1ig"
     TOKEN = os.environ.get("TOKEN", None)
     MONGO_URL = "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority"
     START_PIC = "https://telegra.ph/file/a8ba8edd60489a54f2f84.jpg"
